dfbf0d9907f0e29f3ad5c7d229c72f15583fb045
Raymond Ferrer <rnferrer@ucdavis.edu>
2023-08-21 21:02:51 -0700

    cleaned code in server file

--- server/server.js
+++ server/server.js
@@ -10,9 +10,6 @@ const connectDB = require('./db/connectdb');
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-// const userController = require("./controllers/userController");
-// const recipeController = require("./controllers/recipeController");
-// const collectionController = require("./controllers/collectionController");
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,65 +24,11 @@ const recipeRouter = require('./routes/recipeRouter');
 const collectionRouter = require('./routes/collectionRouter');
 
 app.use("/build", express.static(path.join(__dirname, "../build")));
-// serve index.html on the route '/'
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
-
-// app.get("/signup", (req, res) => {
-//   console.log('hello')
-//   res.sendFile(path.resolve(__dirname, "../client/signup.html"));
-// });
 
 app.use('/api/user', userRouter);
 app.use('/api/recipe', recipeRouter);
 app.use('/api/collection', collectionRouter);
 
-// app.post("/api/login", 
-//   //userController.verifyUser, 
-//   (req, res) => {
-//     console.log('test', req.body)
-//     res.status(200).json(res.locals.isLogged);
-// });
-
-// app.post("/signup", userController.createUser, (req, res) => {
-//   res.status(200).json(res.locals.user)
-// });
-
-// app.get("/findRecipes", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/find.html"));
-// });
-
-// app.post("/findRecipes",
-//   recipeController.saveRecipes,
-//   recipeController.sortRecipes, 
-//   (req, res) => {
-//   res.status(200).json(res.locals.recipes);
-// });
-
-// app.post(
-//   "/collection",
-//   collectionController.createCollection,
-//   (req, res) => {
-//     res.status(200).send(res.locals.collection);
-//   }
-// );
-
-// app.get('/collection', collectionController.generateSavedCollection, (req, res) => {
-//   res.status(200).send('hello')
-// })
-
-// app.delete("/collection", collectionController.deleteCollection, (req, res) => {
-//   res.status(200).send("collection deleted successfully!");
-// });
-
-// app.delete("/recipe", recipeController.deleteRecipe, (req, res) => {
-//   res.status(200).send("recipe deleted successfully!");
-// });
-
-// app.patch("/user", userController.updateMacros, (req, res) => {
-//   res.status(200).send("Macro goal updated successfully!");
-// });
 
 //global error handler
 app.use((err, req, res, next) => {
@@ -100,7 +43,6 @@ app.use((err, req, res, next) => {
   res.status(error.status).json(error.message);
 });
 
-// app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
 const start = async () => {
   try {
